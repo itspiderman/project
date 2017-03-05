331afc8f085d1d62bb907ca77adb63f8fe58ded8
itman020 <itman020@126.com>
2017-03-05 23:30:31 +0800

    jetty debug,servlet demo

---
+++ ichat/src/main/java/com/demo/UserServlet.java
@@ -0,0 +1,59 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		String action=request.getParameter("action");		
+		if("login_input".equals(action)){			
+			request.getRequestDispatcher("login.jsp").forward(request, response);	
+			System.out.println("UserServlet,doGet()"+action);
+		}else if("login".equals(action)){			
+			String name=request.getParameter("name");
+			String password=request.getParameter("password");
+			PrintWriter pw=response.getWriter();
+			//pw.write("name->"+name+",password->"+password);
+			request.getRequestDispatcher("welcome.jsp").forward(request, response);				
+			System.out.println("UserServlet,doGet()"+action);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- ichat/src/main/webapp/index.jsp
+++ ichat/src/main/webapp/index.jsp
@@ -4,6 +4,8 @@
 <br/>
 <a href="demo/calculate.jsp">calculate.jsp</a>
 <br/>
+<a href="/demo/sindex.jsp">go login.jsp</a>
+<br/>
 <a href="#">login.jsp</a>
 </body>
 </html>
