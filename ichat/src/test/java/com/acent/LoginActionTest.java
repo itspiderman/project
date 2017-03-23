dd715ebe1c27d3621f768834232787215e37fc49
itspiderman <itman020@126.com>
2017-03-23 16:42:40 +0800

    Junit demo

---
+++ ichat/src/test/java/com/acent/LoginActionTest.java
@@ -0,0 +1,42 @@
+package com.acent;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class LoginActionTest extends TestCase {
+	LoginAction la;
+
+	@Override
+	public void setUp() throws Exception {
+		la=new LoginAction();
+	}
+
+	@Test
+	public void testGetUserName() {
+		la.setUserName("kevin");
+		assertEquals("kevin",la.getUserName());
+	}
+
+	@Test
+	public void testSetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecute() throws Exception {
+		la.setUserName("kevin");
+		assertEquals("SUCCESS",la.execute());
+	}
+
+}
