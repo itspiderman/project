2d522ce639166045507105fe40bbcba87132b916
itspiderman <itman020@126.com>
2017-04-11 11:32:44 +0800

    hibernate demo

--- ichat/src/main/java/com/services/dao/imp/FundWriteDao.java
+++ ichat/src/main/java/com/services/dao/imp/FundWriteDao.java
@@ -2,6 +2,12 @@ package com.services.dao.imp;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import com.services.dao.IFundWriteDao;
 import com.services.pojo.fund.Fund;
 
@@ -9,8 +15,30 @@ public class FundWriteDao implements IFundWriteDao {
 
 	@Override
 	public int insertFund(Fund fund) {
-		// TODO Auto-generated method stub
-		return 0;
+		SessionFactory sessionFactory = null;
+		Session session=null;
+		final StandardServiceRegistry registry =new StandardServiceRegistryBuilder().configure().build();
+		try{
+			sessionFactory=new MetadataSources(registry).buildMetadata().buildSessionFactory();
+			
+			session=sessionFactory.openSession();
+			session.beginTransaction();
+			session.save(fund);
+			session.getTransaction().commit();
+			session.close();
+			
+			
+		}catch(Exception e){
+			StandardServiceRegistryBuilder.destroy(registry);
+		}finally{
+			if(session!=null){
+				session.close();
+			}
+			if(sessionFactory!=null){
+				sessionFactory.close();
+			}
+		}		
+		return 1;
 	}
 
 	@Override
---
+++ ichat/src/test/java/com/services/dao/imp/FundWriteDaoTest.java
@@ -0,0 +1,54 @@
+package com.services.dao.imp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.services.pojo.fund.Fund;
+
+public class FundWriteDaoTest {
+
+	@Test
+	public void testInsertFundFund() {
+		
+		Fund fd2=new Fund();
+		fd2.setFundUrl("http://fund.eastmoney.com/290004.html");
+		fd2.setFundCode("290004");
+		fd2.setFundName("泰信优质生活混合");
+		fd2.setTypecode('2');
+		FundWriteDao fundWriteDao=new FundWriteDao();
+		int i=fundWriteDao.insertFund(fd2);
+		assertEquals(1, i);
+	}
+
+//	@Test
+//	public void testInsertFundListOfFund() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testUpdateFundString() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testUpdateFundStringArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDeleteFundString() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDeleteFundStringArray() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testUploadFund() {
+//		fail("Not yet implemented");
+//	}
+
+}
