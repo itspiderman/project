816bfd390d6dc75606c692700309f1a598f0b204
itspiderman <itman020@126.com>
2017-09-16 16:37:11 +0800

    fundTool
    create project

---
+++ fundTool/src/com/smeshop/demo/HtmlUnitDemo.java
@@ -0,0 +1,11 @@
+package com.smeshop.demo;
+
+public class HtmlUnitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("htmlunit demo");
+
+	}
+
+}
